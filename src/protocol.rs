da354a6c71cbe5f21cb6098a5c06664f97eae356
Alexander Mills <alex@oresoftware.com>
2026-09-14 18:29:24 -0500

    test: avoid clippy io-other-error in protocol fixtures

--- src/protocol.rs
+++ src/protocol.rs
@@ -214,7 +214,7 @@ mod tests {
         }
 
         fn flush(&mut self) -> io::Result<()> {
-            Err(io::Error::new(io::ErrorKind::Other, "flush failed"))
+            Err(io::Error::other("flush failed"))
         }
     }
 
